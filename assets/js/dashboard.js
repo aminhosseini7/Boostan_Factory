6347442d88a4431ba61bfeb72fb9bd4005948ae9
aminhosseini7 <amin.h76@gmail.com>
2026-09-18 00:46:38 +0330

    Delete assets directory

--- assets/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-fetch('data/database.json')
-.then(r=>r.json())
-.then(db=>{
-document.getElementById('production').innerText=db.production.length;
-document.getElementById('sales').innerText=db.sales.length;
-document.getElementById('customers').innerText=db.customers.length;
-document.getElementById('alerts').innerText=db.alerts.length;
-});
